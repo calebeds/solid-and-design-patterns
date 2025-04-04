d94f5eb0853e0ebc1808126dc4a60381d78d8735
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-04-03 21:37:44 -0300

    add interface segregation principle example

---
+++ src/main/java/interfacesegregationprinciple/InterfaceSegregationPrincipleExample.java
@@ -0,0 +1,13 @@
+package interfacesegregationprinciple;
+
+public class InterfaceSegregationPrincipleExample {
+    public static void main(String[] args) {
+        RotationTree tree = new BalancedTree();
+
+        tree.insert();
+        tree.delete();
+        tree.traverse();
+        tree.leftRotation();
+        tree.rightRotation();
+    }
+}
---
+++ src/main/java/interfacesegregationprinciple/Tree.java
@@ -0,0 +1,8 @@
+package interfacesegregationprinciple;
+
+public interface Tree {
+    void insert();
+    void delete();
+    void traverse();
+
+}
