f3dd77c02a04f32cc8ee054c5f4e3cf9dcd3cf5c
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-04-08 20:35:08 -0300

    add builder pattern example

---
+++ src/main/java/builderpattern/BuilderPatternExample.java
@@ -0,0 +1,8 @@
+package builderpattern;
+
+public class BuilderPatternExample {
+    public static void main(String[] args) {
+        Person person = new Person.Builder("Kevin", "kevin@gmail.com").setAge(15).setAddress("Hudson Street").build();
+        System.out.println(person);
+    }
+}
---
+++ src/main/java/builderpattern/Student.java
@@ -0,0 +1,42 @@
+package builderpattern;
+
+public class Student {
+    private String name;
+    private String university;
+    private int age;
+
+    public Student(Builder builder) {
+        this.name = builder.name;
+        this.university = builder.university;
+        this.age = builder.age;
+    }
+
+    private static class Builder {
+        private final String name;
+        private final String university;
+        private int age;
+
+        public Builder(String name, String university) {
+            this.name = name;
+            this.university = university;
+        }
+
+        private Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        private Student build() {
+            return new Student(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", university='" + university + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
